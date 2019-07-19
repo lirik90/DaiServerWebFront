def863ad3b90fafefa8859dae95f79023038f599
Polina Minkova <polina.mynkova@npmgroup.ru>
2019-07-19 17:09:03 +0700

    Show warn and err statuses

---
+++ src/app/house/house-state/house-state.component.ts
@@ -0,0 +1,50 @@
+/**
+ * Отображает текущий статус бирбокса
+ */
+import {Component, Input, OnInit} from '@angular/core';
+import {HouseService} from '../house.service';
+
+@Component({
+  selector: 'app-house-state',
+  templateUrl: './house-state.component.html',
+  styleUrls: ['./house-state.component.css']
+})
+export class HouseStateComponent implements OnInit {
+
+  constructor(
+    public houseService: HouseService,
+  ) { }
+
+  ngOnInit() {
+  }
+
+  get messages() {
+    const msgArray = [];
+
+    for (const sect of this.houseService.house.sections) {
+      for (const grp of sect.groups) {
+        let status = 'undefined',
+            status_text = '';
+
+        if (grp.status_info !== undefined) {
+          status = grp.status_info.short_text;
+          status_text = grp.status_info.text;
+        }
+
+        if (status === 'Ok') {
+          continue;
+        }
+
+        msgArray.push({
+          section: sect.name,
+          group: grp.title ? grp.title : grp.type.title, // TODO: Why there are 'title' and 'type.title'?
+          status: status,
+          text: status_text,
+        });
+      }
+    }
+
+
+    return msgArray;
+  }
+}
