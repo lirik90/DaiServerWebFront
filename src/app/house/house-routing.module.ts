76296329983c263f83e8a17ddbf4d620ef81e602
Polina Minkova <polina.mynkova@npmgroup.ru>
2019-07-29 12:02:29 +0700

    Guard on routing paths

--- src/app/house/house-routing.module.ts
+++ src/app/house/house-routing.module.ts
@@ -13,6 +13,7 @@ import { LogComponent } from './log/log.component';
 import { ParamComponent } from './param/param.component';
 import {ExportComponent} from './reports/export/export.component';
 import {ReportsModule} from './reports/reports.module';
+import {PermissionGuard} from './permission.guard';
 
 const houseRoutes: Routes = [
   {
@@ -23,20 +24,21 @@ const houseRoutes: Routes = [
         path: ':name',
         component: HouseComponent,
         canActivate: [ProjectLoadGuard],
-        canActivateChild: [AuthGuard, ProjectLoadGuard],
+        canActivateChild: [AuthGuard, ProjectLoadGuard, PermissionGuard],
         children: [
-          { path: '', redirectTo: '/house/:name/view', pathMatch: 'full' },
+          { path: '', redirectTo: 'detail', pathMatch: 'full' },
           { path: 'detail', component: HouseDetailComponent },
           {
             path: 'manage',
             component: ViewComponent,
+            data: { req_perms: ['can_see_more'] },
             children: [
               { path: '', pathMatch: 'full' },
               { path: ':view_id', component: ManageComponent, data: { is_edit: true, is_view: true } }
             ]
           },
-          { path: 'elements', component: ManageComponent, data: { is_edit: true }},
-          { path: 'log', component: LogComponent },
+          { path: 'elements', component: ManageComponent, data: { is_edit: true, req_perms: ['can_see_more'] }},
+          { path: 'log', component: LogComponent, data: { req_perms: ['can_see_more'] }, },
           { path: 'group/:groupId/param', component: ParamComponent },
           {
             path: 'reports',
@@ -51,6 +53,7 @@ const houseRoutes: Routes = [
           {
             path: 'settings',
             loadChildren: 'app/house/settings/settings.module#SettingsModule',
+            data: { req_perms: ['can_see_more'] },
             canLoad: [AuthGuard]
           },
           { path: 'beerbox', loadChildren: 'app/beerbox/beerbox.module#BeerboxModule', canLoad: [AuthGuard] },
