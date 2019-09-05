36416249673d87fc50bc385df0ca55ec61f830f2
Polina Minkova <polina.mynkova@npmgroup.ru>
2019-09-05 13:10:31 +0700

    doc mobile stepper img styles

--- src/app/house/doc/doc.component.ts
+++ src/app/house/doc/doc.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-doc',
   templateUrl: './doc.component.html',
-  styleUrls: ['./doc.component.css']
+  styleUrls: ['./doc.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DocComponent implements OnInit {
 
