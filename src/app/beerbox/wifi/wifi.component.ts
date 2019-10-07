5e04fb26c32106d0e46df1da49d70b7ecfedd4bf
Polina Minkova <polina.mynkova@npmgroup.ru>
2019-10-07 14:15:19 +0700

    wifi interface

--- src/app/beerbox/wifi/wifi.component.ts
+++ src/app/beerbox/wifi/wifi.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HouseService} from '../../house/house.service';
+import {DeviceItem} from '../../house/house';
 
 @Component({
   selector: 'app-wifi',
@@ -7,16 +8,18 @@ import {HouseService} from '../../house/house.service';
   styleUrls: ['./wifi.component.css']
 })
 export class WifiComponent implements OnInit {
-  wifi: any;
+  wifi_pwd: DeviceItem;
+  wifi_ssid: DeviceItem;
 
   constructor(
     private houseService: HouseService,
   ) { }
 
   ngOnInit() {
+    const network = this.houseService.house.sections[0].groups.find(g => g.type.name === 'network').items;
 
-    this.wifi = this.houseService.house.sections[0].groups.find(g => g.type.name === 'network');
-
+    this.wifi_pwd = network.find(i => i.type.name === 'wifi_pwd');
+    this.wifi_ssid = network.find(i => i.type.name === 'wifi_ssid');
   }
 
 }
