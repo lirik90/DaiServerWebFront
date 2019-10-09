27887a342f1ce090f1a72b6aa30d20dc6366c871
Polina Minkova <polina.mynkova@npmgroup.ru>
2019-10-09 11:03:31 +0700

    wifi change mode to manual

--- src/app/beerbox/wifi/wifi.component.ts
+++ src/app/beerbox/wifi/wifi.component.ts
@@ -13,6 +13,7 @@ export class WifiComponent implements OnInit {
   wifi_pwd: DeviceItem;
   wifi_ssid: DeviceItem;
   hasAccess: boolean;
+  networkGroup: Group;
 
   constructor(
     private houseService: HouseService,
@@ -23,13 +24,16 @@ export class WifiComponent implements OnInit {
   ngOnInit() {
     this.hasAccess = this.authService.isFullAccess() || this.authService.isAdmin();
 
-    const network = this.houseService.house.sections[0].groups.find(g => g.type.name === 'network').items;
+    this.networkGroup = this.houseService.house.sections[0].groups.find(g => g.type.name === 'network');
+    const network = this.networkGroup.items;
 
     this.wifi_pwd = network.find(i => i.type.name === 'wifi_pwd');
     this.wifi_ssid = network.find(i => i.type.name === 'wifi_ssid');
   }
 
   save(ssid: HTMLInputElement, pwd: HTMLInputElement) {
+    this.controlService.changeGroupMode(1, this.networkGroup.id)
+
     console.log(ssid.value);
     console.log(pwd.value);
     this.controlService.writeToDevItem(this.wifi_ssid.id, ssid.value);
