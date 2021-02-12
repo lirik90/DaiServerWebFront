0dcf9073b77453808c49518cbdbce55cf1909cf1
Maxim Kavsadze <maksim.lv@mail.ru>
2021-02-12 12:40:09 +0300

    Task 24: complete

--- src/app/scheme/manage-devices/manage-devices.component.ts
+++ src/app/scheme/manage-devices/manage-devices.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from '../../authentication.service';
+import {SchemeService} from '../scheme.service';
+import {Device} from '../scheme';
 
 @Component({
-  selector: 'app-manage-devices',
-  templateUrl: './manage-devices.component.html',
-  styleUrls: ['./manage-devices.component.css']
+    selector: 'app-manage-devices',
+    templateUrl: './manage-devices.component.html',
+    styleUrls: ['./manage-devices.component.css', '../manage/manage.component.css']
 })
 export class ManageDevicesComponent implements OnInit {
+    isEditorModeEnabled = false;
+    isAdmin: boolean;
 
-  constructor() { }
+    devices: Device[];
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private authService: AuthenticationService,
+        private schemeService: SchemeService,
+    ) {
+        this.isAdmin = this.authService.isAdmin();
+        this.devices = this.schemeService.scheme.device;
+    }
+
+    ngOnInit(): void {
+    }
 
 }
