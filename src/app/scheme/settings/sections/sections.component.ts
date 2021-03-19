f4cebb870ca9b7a38aea038db0ed131407fa0ff0
Maxim Kavsadze <maxim1548@gmail.com>
2021-03-19 11:23:18 +0300

    Task 47: fix bug with displaying child params in groups (#46)
    * Task 47: fix bug with displaying child params in groups
    
    * Update src/app/scheme/settings/sections/sections.component.ts
    
    Co-authored-by: Maxim Kavsadze <maksim.lv@mail.ru>
    Co-authored-by: Kirill Zimnikov <kirill@zimnikov.ru>

--- src/app/scheme/settings/sections/sections.component.ts
+++ src/app/scheme/settings/sections/sections.component.ts
@@ -1,115 +1,120 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, OnChanges, SimpleChanges} from '@angular/core';
 
 import {SchemeService} from '../../scheme.service';
 import {Device_Item_Group, DIG_Param, DIG_Param_Type, DIG_Type, Section} from '../../scheme';
 import {SettingsService} from '../../settings.service';
-import {ChangeTemplate, Structure_Type} from '../settings';
+import {ChangeInfo, ChangeState, ChangeTemplate, Structure_Type} from '../settings';
 import {UIService} from '../../../ui.service';
 
 @Component({
-  selector: 'app-sections',
-  templateUrl: './sections.component.html',
-  styleUrls: ['../settings.css', './sections.component.css']
+    selector: 'app-sections',
+    templateUrl: './sections.component.html',
+    styleUrls: ['../settings.css', './sections.component.css']
 })
 export class SectionsComponent extends ChangeTemplate<Section> implements OnInit {
-  constructor(
-    schemeService: SchemeService,
-    private settingsService: SettingsService,
-    ui: UIService,
-  ) {
-    super(schemeService, Section, Structure_Type.ST_SECTION, ui);
-  }
+    constructor(
+        schemeService: SchemeService,
+        private settingsService: SettingsService,
+        ui: UIService,
+    ) {
+        super(schemeService, Section, Structure_Type.ST_SECTION, ui);
+    }
 
-  getObjects(): Section[] {
-    return this.schemeService.scheme.section;
-  }
+    getObjects(): Section[] {
+        return this.schemeService.scheme.section;
+    }
 
-  ngOnInit() {
-    this.fillItems();
+    ngOnInit() {
+        this.fillItems();
 
-    /*
-    // Dialog
-    this.sections = this.sections.filter(s => s !== sct);
-    this.settingsService.deleteSection(sct).subscribe(_ => {
-      // this.controlService.deleteSection(sct);
-    });*/
-  }
+        /*
+        // Dialog
+        this.sections = this.sections.filter(s => s !== sct);
+        this.settingsService.deleteSection(sct).subscribe(_ => {
+          // this.controlService.deleteSection(sct);
+        });*/
+    }
 }
 
 @Component({
-  selector: 'app-groups',
-  templateUrl: './groups.component.html',
-  styleUrls: ['../settings.css', './sections.component.css']
+    selector: 'app-groups',
+    templateUrl: './groups.component.html',
+    styleUrls: ['../settings.css', './sections.component.css']
 })
 export class GroupsComponent extends ChangeTemplate<Device_Item_Group> implements OnInit {
-  @Input() sct: Section;
+    @Input() sct: Section;
 
-  groupTypes: DIG_Type[];
+    groupTypes: DIG_Type[];
 
-  constructor(
-    schemeService: SchemeService,
-    ui: UIService,
-  ) {
-    super(schemeService, Device_Item_Group, Structure_Type.ST_DEVICE_ITEM_GROUP, ui);
-  }
-
-  getObjects(): Device_Item_Group[] {
-    return this.sct.groups;
-  }
-
-  ngOnInit() {
-    this.groupTypes = this.schemeService.scheme.dig_type;
-    this.fillItems();
-  }
-
-  title(item: Device_Item_Group = undefined): string
-  {
-    if (item === undefined)
-    {
-      item = this.sel_item.obj;
+    constructor(
+        schemeService: SchemeService,
+        ui: UIService,
+    ) {
+        super(schemeService, Device_Item_Group, Structure_Type.ST_DEVICE_ITEM_GROUP, ui);
     }
-    return item.title ? item.title : (item.type ? item.type.title : '');
-  }
 
-  initItem(obj: Device_Item_Group): void
-  {
-    obj.section_id = this.sct.id;
-  }
+    getObjects(): Device_Item_Group[] {
+        return this.sct.groups;
+    }
+
+    ngOnInit() {
+        this.groupTypes = this.schemeService.scheme.dig_type;
+        this.fillItems();
+    }
+
+    title(item: Device_Item_Group = undefined): string {
+        if (item === undefined) {
+            item = this.sel_item.obj;
+        }
+        return item.title ? item.title : (item.type ? item.type.title : '');
+    }
+
+    initItem(obj: Device_Item_Group): void {
+        obj.section_id = this.sct.id;
+    }
 }
 
 @Component({
-  selector: 'app-params-in-group',
-  templateUrl: './params-in-group.component.html',
-  styleUrls: ['../settings.css', './sections.component.css']
+    selector: 'app-params-in-group',
+    templateUrl: './params-in-group.component.html',
+    styleUrls: ['../settings.css', './sections.component.css']
 })
-export class ParamsInGroupComponent extends ChangeTemplate<DIG_Param> implements OnInit
-{
-  @Input() group: Device_Item_Group;
+export class ParamsInGroupComponent extends ChangeTemplate<DIG_Param> implements OnChanges {
+    @Input() group: Device_Item_Group;
 
-  params: DIG_Param_Type[];
+    params: DIG_Param_Type[];
 
-  constructor(
-    schemeService: SchemeService,
-    ui: UIService,
-  ) {
-    super(schemeService, DIG_Param, Structure_Type.ST_DIG_PARAM, ui);
-  }
+    constructor(
+        schemeService: SchemeService,
+        ui: UIService,
+    ) {
+        super(schemeService, DIG_Param, Structure_Type.ST_DIG_PARAM, ui);
+    }
 
-  getObjects(): DIG_Param[]
-  {
-    return this.group.params;
-  }
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes.group) {
+            this.fillItems();
+            this.params = this.schemeService.scheme.dig_param_type.filter(obj => obj.group_type_id === this.group.type_id);
+        }
+    }
 
-  ngOnInit()
-  {
-    this.fillItems();
-    this.params = this.schemeService.scheme.dig_param_type.filter(obj => obj.group_type_id === this.group.type_id);
-  }
+    getObjects(): DIG_Param[] {
+        const arr: DIG_Param[] = [];
+        const add = (items: DIG_Param[]) => {
+            for (const item of items) {
+                arr.push(item);
+                if (item.childs) {
+                    add(item.childs);
+                }
+            }
+        };
+        
+        add(this.group.params);
+        return arr;
+    }
 
-  initItem(obj: DIG_Param): void
-  {
-    obj.param = new DIG_Param_Type();
-    obj.group_id = this.group.id;
-  }
+    initItem(obj: DIG_Param): void {
+        obj.param = new DIG_Param_Type();
+        obj.group_id = this.group.id;
+    }
 }
-
