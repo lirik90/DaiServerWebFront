fef2c23158e8c8f3754c811c42cd3e00066b3005
Maksim Kavsadze <maxim1548@gmail.com>
2021-05-31 22:00:25 +0300

    Task 69: show axis only if dataset visible

--- src/app/scheme/reports/charts/chart-item/chart-item.component.ts
+++ src/app/scheme/reports/charts/chart-item/chart-item.component.ts
@@ -35,10 +35,12 @@ export class ChartItemComponent implements OnInit, OnChanges, DoCheck {
         id: 'A',
         type: 'linear',
         position: 'left',
+        display: 'auto',
     }, {
         id: 'B',
         type: 'linear',
         position: 'right',
+        display: 'auto',
         ticks: {
             max: 2,
             min: -1,
@@ -82,8 +84,7 @@ export class ChartItemComponent implements OnInit, OnChanges, DoCheck {
     @Output() axeChange: EventEmitter<BuiltChartParams> = new EventEmitter();
     @Output() built: EventEmitter<BuiltChartParams> = new EventEmitter();
 
-    update(): void
-    {
+    update(): void {
         if (!this.chart || !this.chart.chart) return;
 
         this.zone.run(() => this.chart.chart.update());
@@ -249,6 +250,12 @@ export class ChartItemComponent implements OnInit, OnChanges, DoCheck {
 
         console.log('Value dropped', log);
         return null;
+    }
+
+    toggleAxisVisibility(axisId: string, v: boolean) {
+        const axe = this.options.scales.yAxes.find(axe => axe.id === axisId);
+        console.log(axisId, v, axe);
+        axe.display = v;
     }
 
     addData(dataPack: Paginator_Chart_Value, data_param_name: string, additional = false): void
