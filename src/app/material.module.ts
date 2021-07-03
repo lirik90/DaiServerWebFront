98b13605d5af88f5e33d7e18238b1cb2c01dfa57
Кирилл Зимников <kirill@zimnikov.ru>
2021-07-03 11:17:05 +0700

    Update..

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 
-import { ChartModule } from 'angular2-chartjs';
-
 import { MatButtonModule } from '@angular/material/button';
-import { 
+import {
 //  MatAutocompleteModule,
 //  MatButtonToggleModule,
 MatCardModule } from '@angular/material/card';
@@ -11,20 +9,20 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
-import { 
+import {
 //  MatChipsModule,
 //  MatDividerModule,
 MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { 
+import {
 //  MatGridListModule,
 MatListModule } from '@angular/material/list';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
-import { 
+import {
 //  MatMenuModule,
 //  MatProgressBarModule,
 //  MatRippleModule,
@@ -35,7 +33,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSortModule } from '@angular/material/sort';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatTableModule } from '@angular/material/table';
-import { 
+import {
 //  MatTabsModule,
 MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -43,8 +41,6 @@ import { CdkTableModule } from '@angular/cdk/table';
 
 @NgModule({
   exports: [
-    ChartModule,
-
     MatButtonModule,
     MatTooltipModule,
     MatSlideToggleModule,
